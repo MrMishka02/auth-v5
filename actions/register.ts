654b38694bbeab5789a899ea8c46baff99aa4c28
Mikheil Gogia <m.gogia@hotmail.com>
2024-01-10 23:10:55 +0400

    docs: added bcrypt fro password and create new user in db

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import * as z from "zod"
 import { RegisterSchema } from "@/schemas"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db"
 
 const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -10,7 +12,28 @@ const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" }
   }
 
-  return { success: "Email sent!" }
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    }
+  })
+
+  if (existingUser) {
+    return { error: "Email already is use!" }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  })
+
+  return { success: "User created!" }
 }
 
 export default register
