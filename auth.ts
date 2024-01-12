e6df54baacda273b677554e6bbec1da995a031a9
Mikheil Gogia <m.gogia@hotmail.com>
2024-01-12 23:12:24 +0400

    chore: added role in auth

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import authConfig from "./auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/lib/db"
+import { getUserById } from "@/data/user"
 
 export const {
   handlers: { GET, POST },
@@ -15,9 +16,19 @@ export const {
         session.user.id = token.sub
       }
 
+      if (token.role && session.user) {
+        session.user.role = token.role
+      }
+
       return session
     },
     async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+      token.role = existingUser.role
+
       return token
     }
   },
