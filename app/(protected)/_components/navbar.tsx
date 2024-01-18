e08804c2c2147be21cf45651dca30678eb716809
Mikheil Gogia <m.gogia@hotmail.com>
2024-01-19 01:38:22 +0400

    docs: added dropdown menu and avatar on settings page.

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import UserButton from "@/components/auth/user-button";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -35,7 +36,7 @@ const Navbar = () => {
           <Link href="/settings">Settings</Link>
         </Button>
       </div>
-      <p>User Button</p>
+      <UserButton />
     </nav>
   );
 };
