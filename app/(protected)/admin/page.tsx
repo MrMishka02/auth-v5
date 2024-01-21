1a4327f3bcf0f8c835dd533f96538ea148f75d88
Mikheil Gogia <m.gogia@hotmail.com>
2024-01-21 23:02:17 +0400

    docs: added toaster from shadcn, added success and error messages on use roles

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+import { admin } from "@/actions/admin";
 import { RoleGate } from "@/components/auth/role-gate";
 import FormSuccess from "@/components/form-success";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
+import { toast } from "sonner";
 
 const AdminPage = () => {
+  const onServerActionClick = () => {
+    admin().then((data) => {
+      if (data.success) {
+        toast.success(data.success);
+      }
+      if (data.error) {
+        toast.error(data.error);
+      }
+    });
+  };
+
   const onApiRouteClick = () => {
     fetch("/api/admin").then((response) => {
       if (response.ok) {
-        console.log("OKAY");
+        toast.success("Allowed API Route!");
       } else {
-        console.error("FORBIDDEN");
+        toast.error("Forbidden API Route!");
       }
     });
   };
@@ -32,7 +45,7 @@ const AdminPage = () => {
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
           <p className="text-sm font-medium">Admin-only Server Action</p>
-          <Button>Click to test</Button>
+          <Button onClick={onServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
