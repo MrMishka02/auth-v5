d9cda9d3d8538e37759331049cf999fc89e742c2
Mikheil Gogia <m.gogia@hotmail.com>
2024-01-25 23:56:37 +0400

    chore: corrected two factor verification

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -46,6 +46,7 @@ const SettingsPage = () => {
       password: undefined,
       newPassword: undefined,
       role: user?.role || undefined,
+      isTwoFactorEnabled: user?.isTwoFactorEnabled || undefined,
     },
   });
 
